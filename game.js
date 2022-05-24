c530feacab44cd5228388c08b59bff5d10adcbcd
Mohamad Muradi <mohamadmuradi@Mohamads-MacBook-Air.local>
2022-05-24 09:42:44 +0300

    JS: Added global variables

---
+++ game.js
@@ -0,0 +1,28 @@
+window.onload=function(){
+
+    var playing = false;
+    var score = 0;
+
+    const start_btn = document.getElementById("start");
+    const end_btn = document.getElementById("end");
+    const game_zone = document.getElementById("game");
+    const live_element = document.getElementById("live");
+    const last_element = document.getElementById("last");
+    const best_element = document.getElementById("best");
+    const status = document.getElementById("status");
+
+    const boundaries = Array.from(document.getElementsByClassName("boundary"));
+    const score_board = Array.from(document.getElementsByClassName("example"))[0];
+
+    const game_title = document.getElementsByTagName("h1")[0]
+
+    const grey_background = boundaries[0].style.backgroundColor;
+
+    var milliseconds = 0;
+    var seconds = 0;
+    var minutes = 0;
+    var timer = null;
+
+    var best = [9999999,9999999,9999999];
+
+}
